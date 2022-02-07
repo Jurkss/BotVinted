1203649bfa883d3fa597fea6905d4bb10a53af82
Jurkss <99148834+Jurkss@users.noreply.github.com>
2022-02-07 01:09:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Discord.Client({
 const synchronizeSlashCommands = require('discord-sync-commands');
 synchronizeSlashCommands(client, [
 {
-        name: 'botinstall',
+        name: 'abonner',
         description: 'Abonnez-vous à une URL de recherche',
         options: [{
                 name: 'url',
